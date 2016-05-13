19612e2692d8b1d42b6975d030bf7f76202368e6
wlq <137788388@qq.com>
2016-05-13 14:54:11 +0800

    Alexander.java

---
+++ Alexander.java
@@ -0,0 +1,13 @@
+class alexdaner{
+	private String name;
+	private String type;
+	public static void main(){
+		alexdaner a = new alexdaner(); 
+	}
+	public void setname(String name){
+		this.name=name;
+	}
+	public String getname(){
+		retrun name;
+	}
+}
